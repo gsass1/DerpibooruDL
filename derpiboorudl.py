d6289baf87de10a93a3b642d8765b2163fdbe1bf
Gian Sass <gian.sass@outlook.de>
2014-10-27 15:23:50 +0100

    Cleanup and use os.path.join

--- derpiboorudl.py
+++ derpiboorudl.py
@@ -15,14 +15,15 @@ def downloadallimages(images):
         url = image["image"]
         url = "https:" + url
         filename = os.path.basename(url)
-        filepath = destdir + "/" + filename
+        filepath = os.path.join(destdir, filename)
+        print(filepath)
         if not os.path.isfile(filepath):
             print("Downloading %s" % url)
             urllib.request.urlretrieve(url, filepath)
             if shortfilenames:
                 filetype = os.path.splitext(filepath)[1]
-                os.rename(filepath, destdir + "/" + id + filetype)
-            
+                os.rename(filepath, os.path.join(destdir, id + filetype))
+
 def stringwithnoquotes(string):
     if string.startswith('"') and string.endswith('"'):
         string = string[1:-1]
@@ -32,19 +33,25 @@ apikey = os.getenv("DERPIBOORUAPIKEY")
 if apikey is None:
     print("No API key was set! (DERPIBOORUAPIKEY)")
     apikey = ""
+
 if len(sys.argv) < 3:
     print("Usage: %s <destdir> <tagname> <maxpages>" % sys.argv[0])
     exit()
+
 destdir = stringwithnoquotes(sys.argv[1])
-# Remove last character its a slash
+
+# Remove last character if its a slash
 destdir = destdir.rstrip("/")
 searched_tag = stringwithnoquotes(sys.argv[2])
+
 if len(sys.argv) > 3:
     maxpages = int(sys.argv[3])
+
 if len(sys.argv) > 4:
     for arg in sys.argv[4:]:
         if arg == "-s" or arg == "--shortnames":
             shortfilenames = True
+
 for i in range(1, maxpages):
     url = "https://derpiboo.ru/search.json?q=%s&page=%d&key=%s" % (searched_tag, i, apikey)
     print("Searching page %d" % i)
