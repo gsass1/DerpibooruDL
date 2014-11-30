e1d44ad4e3de2e1a05a0e5423dda93b9e16ac67f
Joshua Stone <joshua.gage.stone@gmail.com>
2014-11-30 15:41:20 -0500

    Add in-memory sha512 checksumming to ensure corrupted files don't get saved

--- derpiboorudl.py
+++ derpiboorudl.py
@@ -2,6 +2,7 @@
 
 from derpibooru import Search
 from requests import get, codes
+from hashlib import sha512
 
 import argparse
 import logging
@@ -11,15 +12,17 @@ import sys
 
 logger = None
 
-def download_file(url, fname):
-    r, chunk_size = get(url, stream=True), 1024
+def sha512_hash(f):
+    h = sha512()
+
+    h.update(f)
+    return h.hexdigest()
+
+def download_file(url):
+    r = get(url)
 
     if r.status_code == codes.ok:
-        with open(fname, "wb") as f:
-            for chunk in r.iter_content(chunk_size): 
-                f.write(chunk)
-
-        return fname
+        return r.content
 
 def setup_logger(log):
     log.setLevel(logging.DEBUG)
@@ -61,7 +64,18 @@ def main():
         path = os.path.join(destdir, filename)
         if not os.path.isfile(path):
             logger.info("Now downloading image with id {0}".format(image.id_number))
-            download_file(image.full, path)            
+
+            download = download_file(image.full)
+            if download:
+                if image.sha512_hash == sha512_hash(download):
+                    with open(path, "wb") as f:
+                        f.write(download)
+                else:
+                    logger.error("sha512 hashes for {0} don't match up".format(image.id_number))
+            else:
+                logger.error("download for {0} didn't complete".format(image.id_number))
+
 
 if __name__ == "__main__":
     main()
+
