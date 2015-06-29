522d1da0a2a4a3eeb451572aab67a7682cdc6fa5
Gian Sass <gian.sass@outlook.de>
2015-06-29 15:03:22 +0200

    Add multithreading option. Needs code review.

--- derpiboorudl.py
+++ derpiboorudl.py
@@ -5,10 +5,13 @@ from requests import get, codes
 from hashlib import sha512
 
 import argparse
+import copy
 import logging
 import os
 import os.path
 import sys
+import threading
+import time
 
 logger = None
 
@@ -34,6 +37,32 @@ def setup_logger(log):
     ch.setFormatter(formatter)
     log.addHandler(ch)
 
+def download_images(search, destdir, verification, logger):
+    for image in search:
+        filename = os.path.basename(image.full)
+        path = os.path.join(destdir, filename)
+        if os.path.isfile(path):
+            continue
+        logger.info("Downloading image %d" % image.id_number)
+        download = download_file(image.full)
+        if verification:
+            if image.sha512_hash == sha512_hash(download):
+                with open(path, "wb") as f:
+                    f.write(download)
+            else:
+                logger.error("sha512 hashes for {0} don't match up".format(image.id_number))
+        else:
+            with open(path, "wb") as f:
+                f.write(download)
+
+def chunks(seq, num):
+  avg = len(seq) / float(num)
+  last = 0.0
+
+  while last < len(seq):
+    yield seq[int(last):int(last + avg)]
+    last += avg
+
 def main():
     logger = logging.getLogger("derpiboorudl")
     setup_logger(logger)
@@ -45,9 +74,10 @@ def main():
     parser.add_argument("-c", "--count", default=100, help="The count of images you wish to download", type=int)
     parser.add_argument("-k", "--key", default=None, help="Specify the API key (normally present as env variable)")
     parser.add_argument("-nv", "--noverification", action='store_false', default=True, help="Disable image SHA512 verification")
+    parser.add_argument("-t", "--threads", default=1, help="Amount of threads to create", type=int)
     args = parser.parse_args()
 
-    destdir, query, maximages, verification = args.destdir, args.query, args.count, args.noverification
+    destdir, query, maximages, verification, threadcount = args.destdir, args.query, args.count, args.noverification, args.threads
  
     # Read API key from --key if present or else read from env
     apikey = args.key if args.key else os.getenv("DERPIBOORUAPIKEY")
@@ -60,22 +90,27 @@ def main():
     if not os.path.isdir(destdir):
         os.mkdir(destdir)
 
-    for image in search:
-        filename = os.path.basename(image.full)
-        path = os.path.join(destdir, filename)
-        if not os.path.isfile(path):
-            logger.info("Now downloading image with id {0}".format(image.id_number))
+    if threadcount > 1:
+        logger.info("Using %d threads" % threadcount)
 
-            download = download_file(image.full)
-            if verification:
-                if image.sha512_hash == sha512_hash(download):
-                    with open(path, "wb") as f:
-                        f.write(download)
-                else:
-                    logger.error("sha512 hashes for {0} don't match up".format(image.id_number))
-            else:
-                with open(path, "wb") as f:
-                    f.write(download)
+    # Copy the search object to a valid list
+    # FIXME: we should be able to do this without copying the images
+    images = []
+    for image in search:
+        images.append(copy.deepcopy(image))
+
+    threads = []
+    for c in chunks(images, threadcount):
+        t = threading.Thread(target = download_images, args = (c, destdir, verification, logger))
+        threads.append(t)
+        t.daemon = True
+        t.start()
+
+    while threading.active_count() -1 > 0:
+        time.sleep(0.1)
+
+    for t in threads:
+        t.join()
 
 if __name__ == "__main__":
     main()
